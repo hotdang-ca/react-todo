4f1b660213abd83b6d2a1d5ca7675efdee2761f9
James Robert Perih <james@hotdang.ca>
2016-12-11 00:14:22 -0600

    MinHeight scroller

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -24,7 +24,7 @@ var TodoList = React.createClass({
         };
 
         return (
-            <div>
+            <div className="container__list">
                 {renderTodos()}
             </div>
         );
